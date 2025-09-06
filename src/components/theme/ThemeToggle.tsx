a6d3e25517bdc0bb71abccb21d0f94be6fc34829
Prasad Silva <prasad@bitsemble.com>
2025-09-06 19:30:38 -0400

    make theme toggle styleable

--- src/components/theme/ThemeToggle.tsx
+++ src/components/theme/ThemeToggle.tsx
@@ -1,11 +1,12 @@
 import { Button } from '@/components/ui/button';
 import { Moon, Sun } from 'lucide-react';
+import type { ComponentProps } from 'react';
 import { useTheme } from './hooks';
 
-export function ThemeToggle() {
+export function ThemeToggle({ className }: ComponentProps<typeof Button>) {
   const { theme, setTheme } = useTheme();
   return (
-    <Button variant="outline" size="icon_sm" onClick={() => setTheme(theme == 'dark' ? 'light' : 'dark')}>
+    <Button variant="outline" size="icon_sm" onClick={() => setTheme(theme == 'dark' ? 'light' : 'dark')} className={className}>
       {theme == 'dark' ? <Sun /> : <Moon />}
     </Button>
   );
