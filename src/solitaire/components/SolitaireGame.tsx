ae2b70f868cd8b1d6ff7d2b9fc58e7c1aa52100f
Prasad Silva <prasad@bitsemble.com>
2025-09-07 17:26:34 -0400

    minor refactor

--- src/solitaire/components/SolitaireGame.tsx
+++ src/solitaire/components/SolitaireGame.tsx
@@ -24,13 +24,13 @@ function SolitaireGame() {
 
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <Dialog>
-        <div id="viewport" className="absolute w-screen h-screen min-w-[820px] bg-background flex flex-col">
-          <div
-            id="title-bar"
-            className="w-full h-fit bg-neutral-200 dark:bg-neutral-700 shadow-sm shadow-gray-500 dark:shadow-gray-800 flex flex-row justify-center"
-          >
-            <div id="title-bar-content" className="w-[820px] flex flex-row gap-2 p-2 items-center">
+      <div id="viewport" className="absolute w-screen h-screen min-w-[820px] bg-background flex flex-col">
+        <div
+          id="title-bar"
+          className="w-full h-fit bg-neutral-200 dark:bg-neutral-700 shadow-sm shadow-gray-500 dark:shadow-gray-800 flex flex-row justify-center"
+        >
+          <div id="title-bar-content" className="w-[820px] flex flex-row gap-2 p-2 items-center">
+            <Dialog>
               <DialogTrigger asChild>
                 <InfoToggle className="w-9 h-9" />
               </DialogTrigger>
@@ -47,24 +47,24 @@ function SolitaireGame() {
                   .
                 </span>
               </DialogContent>
-              <div className="flex-2">React Solitaire</div>
-              <Button onClick={startNewGame} variant="outline">
-                New Game
-              </Button>
-              <ThemeToggle className="w-9 h-9" />
-            </div>
-          </div>
-          <div id="canvas-container" className="flex-2 flex flex-row justify-center">
-            <div id="canvas-area" className="w-[820px]">
-              <PlayingCardsContext value={createNewPlayingCardsContextValue()}>
-                <SolitaireContext value={solitaireGame}>
-                  <SolitaireCanvas />
-                </SolitaireContext>
-              </PlayingCardsContext>
-            </div>
+            </Dialog>
+            <div className="flex-2">React Solitaire</div>
+            <Button onClick={startNewGame} variant="outline">
+              New Game
+            </Button>
+            <ThemeToggle className="w-9 h-9" />
           </div>
         </div>
-      </Dialog>
+        <div id="canvas-container" className="flex-2 flex flex-row justify-center">
+          <div id="canvas-area" className="w-[820px]">
+            <PlayingCardsContext value={createNewPlayingCardsContextValue()}>
+              <SolitaireContext value={solitaireGame}>
+                <SolitaireCanvas />
+              </SolitaireContext>
+            </PlayingCardsContext>
+          </div>
+        </div>
+      </div>
     </ThemeProvider>
   );
 }
